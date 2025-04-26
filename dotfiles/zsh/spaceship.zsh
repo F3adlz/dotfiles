fe6afabbf3c9d92f9616f16a4b195cd9ad1e5946
Kirill Fedorov <kfedorov@ibsys.ru>
2025-04-26 18:12:06 +0300

    migrate to antigen zsh plugin manager

---
+++ dotfiles/zsh/spaceship.zsh
@@ -0,0 +1,13 @@
+spaceship remove java
+spaceship remove package
+spaceship remove docker
+
+spaceship add docker_context
+
+SPACESHIP_GIT_PREFIX="%F{magenta}(%f"
+SPACESHIP_GIT_SUFFIX="%F{magenta})%f "
+SPACESHIP_GIT_SYMBOL=""
+
+# https://spaceship-prompt.sh/sections/docker/#Docker-context-docker_context
+SPACESHIP_DOCKER_CONTEXT_PREFIX="on "
+SPACESHIP_DOCKER_CONTEXT_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
